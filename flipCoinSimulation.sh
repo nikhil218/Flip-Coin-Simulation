0fcc08f84780f0a8b94acc05be9f38bfdcf5f8fa
nikhil218 <nikhil.tirpude317@gmail.com>
2021-04-23 12:22:31 +0530

    total number of times each won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -x
 
-coinFlip=$((RANDOM%2))
+echo "Welcome to Flip Coin Simulator Problem!"
 
-if [[$coinFlip -eq 0]]
-then
-	echo "heads"
-else
-	echo "tails"
-fi
+limit=21
+Heads=0
+Tails=0
+
+while [[ $Heads != $limit && $Tails != $limit ]]
+do
+	a=$((RANDOM%2))
+
+	if [[ $a -eq 0 ]]
+	then
+	        Heads=$(($Heads+1))
+	else
+	        Tails=$(($Tails+1))
+	fi
+done
+
+echo "Number of times Heads won is : " $Heads
+echo "Number of times Tails won is : " $Tails
